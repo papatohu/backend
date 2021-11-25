045e6e5231a96c8eed4552e66cf217e7da2e45c0
TheRaynex <rafael.luedtke@web.de>
2021-11-25 19:58:28 +0100

    Spring web setup

---
+++ src/main/java/com/wordpress/papatohu/papatohubackend/PapatohuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.papatohu.papatohubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapatohuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapatohuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wordpress/papatohu/papatohubackend/PapatohuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.papatohu.papatohubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapatohuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
