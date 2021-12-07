28c174b5e8b038fc2db4c77e29dc38a258931946
TheRaynex <rafael.luedtke@web.de>
2021-12-07 13:37:42 +0100

    Rest Controller is now connected to the DB and can create new User Configs and can return one, by id.

---
+++ src/main/java/com/wordpress/papatohu/papatohubackend/PapatohuRestAPI.java
@@ -0,0 +1,37 @@
+package com.wordpress.papatohu.papatohubackend;
+
+import com.wordpress.papatohu.papatohubackend.model.UConfig;
+import com.wordpress.papatohu.papatohubackend.repository.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@EnableMongoRepositories
+@RestController
+public class PapatohuRestAPI {
+
+    @Autowired
+    ItemRepository UConfigRepo;
+
+    Logger logger = LoggerFactory.getLogger(PapatohuRestAPI.class);
+
+    @PostMapping("/newUser")
+    public UConfig conf(@RequestBody UConfig config) {
+        config.setId(UUID.randomUUID().toString());
+        UConfigRepo.save(config);
+        logger.info("created new User Config");
+        return config;
+    }
+
+    @GetMapping("/get/{id}")
+    public UConfig getConfig(@PathVariable String id) {
+        UConfig item = UConfigRepo.findItemByID(id);
+        logger.info("request user config");
+        return item;
+    }
+
+}
